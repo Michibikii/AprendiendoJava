fc78f3e2f67ec9321299840c3311fc43b1727f1f
Agustin Duarte <agusleanduarte@gmail.com>
2025-09-03 18:15:19 -0300

    Añadido Ejercicio2

---
+++ src/test/java/TrabajoPractico1/Ejercicio2.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TrabajoPractico1;
+
+/**
+ *
+ * @author Agustin Duarte
+ */
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese cantidad de notas: ");
+        int n = sc.nextInt();
+        int[] notas = new int[n];
+        int aprobados = 0, desaprobados = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nota " + (i + 1) + ": ");
+            notas[i] = sc.nextInt();
+            if (notas[i] >= 6) aprobados++;
+            else desaprobados++;
+        }
+
+        System.out.println("Aprobados: " + aprobados);
+        System.out.println("Desaprobados: " + desaprobados);
+
+        sc.close();
+    }
+}
